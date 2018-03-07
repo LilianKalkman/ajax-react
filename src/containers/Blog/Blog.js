8ae1f0f54994169655fb6360d9e1730b040ab432
LilianKalkman <liliankalkman@hotmail.com>
2018-03-07 13:27:28 +0100

    fetch fullpost

--- src/containers/Blog/Blog.js
+++ src/containers/Blog/Blog.js
@@ -9,29 +9,40 @@ import './Blog.css';
 
 class Blog extends Component {
   state = {
-    posts: []
+    posts: [],
+    selectedPost: null
   }
-  componentDidMount(){
-    axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
-      const posts = response.data.slice(0, 4);
-      const updatedPosts = posts.map( post => {
-        return {...post,
-        author: 'Max'}
+    componentDidMount(){
+      axios.get('https://jsonplaceholder.typicode.com/posts').then(response => {
+        const posts = response.data.slice(0, 4);
+        const updatedPosts = posts.map( post => {
+          return {...post,
+          author: 'Max'}
+        });
+        this.setState({ posts: updatedPosts});
       });
-      this.setState({ posts: updatedPosts});
-    });
-  }
+    }
+
+    selectedPostIdHandler = (id) => {
+      this.setState({selectedPost: id});
+    }
 
     render () {
       const posts = this.state.posts.map( post => {
-      return <Post key={post.id} title={post.title} author={post.author}/>})
+      return <Post
+        key={post.id}
+        title={post.title}
+        author={post.author}
+        clicked={() => this.selectedPostIdHandler(post.id)}/>
+    });
+
         return (
             <div>
                 <section className="Posts">
                   {posts}
                 </section>
                 <section>
-                    <FullPost />
+                    <FullPost id={this.state.selectedPost}/>
                 </section>
                 <section>
                     <NewPost />
